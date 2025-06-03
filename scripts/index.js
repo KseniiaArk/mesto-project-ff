457ee767f63e57dfe165850d509d3dc98b59606c
Ksenia <mogilevicsena@gmail.com>
2025-06-03 13:43:41 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+function createCard(cardData, cardDelete) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    deleteButton.addEventListener('click', () => cardDelete(cardElement));
+
+    return cardElement;
+}
+
+function handleDelete(cardElement) {
+    cardElement.remove();
+}
+
+function drawCards(cardsArray, container) {
+  cardsArray.forEach(cardData => {
+    const cardElement = createCard(cardData, handleDelete);
+    container.append(cardElement);
+  });
+}
+
+drawCards(initialCards, cardList);
